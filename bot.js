68c5712d9e41ecd0cb99d9060600fce0091f856d
mezooo99 <62220445+mezooo99@users.noreply.github.com>
2020-03-25 14:06:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== "571251253246427139") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
